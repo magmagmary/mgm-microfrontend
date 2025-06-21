052151ea6d7331311b4336f1d05c9aa00b5b08c8
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-06-21 20:21:34 +0200

    feat: cart app

---
+++ cart/webpack.config.js
@@ -0,0 +1,42 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+
+
+const config = {
+    mode:'development',
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',  
+    },
+    module:{
+        rules: [
+            {
+              test: /\.(js|ts)x?$/,
+              use: 'ts-loader',
+              exclude: /node_modules/,
+            },
+          ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devServer: {
+        port: 8002,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            hash: true,
+        }),
+        new webpack.container.ModuleFederationPlugin({
+            name: 'products',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './ProductsIndex': './src/index.ts',
+            },
+        })
+    ],
+
+}
+
+export default config;
