14259ad7f134af5bd795a08d9008021ddb2622ab
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-07-19 14:31:04 +0200

    chore: test commit

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -2,10 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { createBrowserRouter } from "react-router-dom";
 import Marketing from "./components/Marketing";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Marketing /> },
-  { path: "/test", element: <h1>Test</h1> },
-]);
+const router = createBrowserRouter([{ path: "/", element: <Marketing /> }]);
 
 const App = () => {
   return <RouterProvider router={router} />;
