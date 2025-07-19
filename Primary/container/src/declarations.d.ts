a17864363ad483dd4d642343df430501e3b2f747
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-07-19 11:32:41 +0200

    feat: restructure

--- Primary/container/src/declarations.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'products/ProductsIndex' {
-    export function mount(el: HTMLElement): void;
-}
-
-declare module 'cart/cartIndex' {
-    export function mount(el: HTMLElement): void;
-}
-
-
