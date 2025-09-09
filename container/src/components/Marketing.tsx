b3e09cd20162ce067cf75d597be3e49d6e3dd924
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-09-09 09:27:30 +0200

    chore: add navigate condition for browser history

--- container/src/components/Marketing.tsx
+++ container/src/components/Marketing.tsx
@@ -1,15 +1,18 @@
 import { mount } from "marketing/MarketingApp";
 import { useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Marketing = () => {
   const ref = useRef(null);
   const navigate = useNavigate();
+  const {pathname:currentPathname} = useLocation();
 
   useEffect(() => {
     mount(ref.current as unknown as HTMLElement , {
       onNavigate: (location:{pathname:string}) => {
-       navigate(location.pathname)
+        if (location.pathname !== currentPathname) {
+          navigate(location.pathname)
+        }
       },
     });
   }, []);
