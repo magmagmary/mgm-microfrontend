cb53cc08a587cc979bc7833706fe19ae4dc888b3
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-09-10 11:34:39 +0200

    chore: handle protected route

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,7 +1,7 @@
-import { Outlet, RouterProvider } from "react-router-dom";
+import { Navigate, Outlet, RouterProvider } from "react-router-dom";
 import { createBrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
-import { createContext, lazy, Suspense, useState } from "react";
+import { createContext, lazy, Suspense, useMemo, useState } from "react";
 
 const Auth = lazy(() => import("./components/Auth"));
 const Marketing = lazy(() => import("./components/Marketing"));
@@ -9,34 +9,36 @@ const Dashboard = lazy(() => import("./components/Dashboard"));
 
 const AuthContext = createContext<{signedIn:boolean,setSignedIn:(value:boolean) => void} | null>(null);
 
-const router = createBrowserRouter([
-  {
-    element: (
-      <div className="w-svw">
-        <Header />
-        <Outlet />
-      </div>
-    ),
-    children: [
-      {
-        path: "/*",
-        element: <Suspense fallback={<div>Loading...</div>}><Marketing /></Suspense>,
-      },
-      {
-        path: "/auth/*",
-        element: <Suspense fallback={<div>Loading...</div>}><Auth /></Suspense>,
-      },
-      {
-        path: "/dashboard",
-        element: <Suspense fallback={<div>Loading...</div>}><Dashboard /></Suspense>,
-      }
-    ],
-  },
-  
-]);
-
 const App = () => {
   const [signedIn, setSignedIn] = useState(false);
+
+  const router = useMemo(()=>{
+   return createBrowserRouter([
+      {
+        element: (
+          <div className="w-svw">
+            <Header />
+            <Outlet />
+          </div>
+        ),
+        children: [
+          {
+            path: "/*",
+            element: <Suspense fallback={<div>Loading...</div>}><Marketing /></Suspense>,
+          },
+          {
+            path: "/auth/*",
+            element: <Suspense fallback={<div>Loading...</div>}><Auth /></Suspense>,
+          },
+          {
+            path: "/dashboard",
+            element: signedIn ? <Suspense fallback={<div>Loading...</div>}><Dashboard /></Suspense> : <Navigate to="/auth/login" />,
+          }
+        ],
+      },
+      
+    ])
+  }, [signedIn]);
 
   return <AuthContext.Provider value={{ signedIn, setSignedIn }}>
     <RouterProvider router={router} />
